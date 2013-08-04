fb5049a15610dd7c441f103d129f4cdb7e46350c
A R K <shemer77@gmail.com>
2013-08-04 11:40:02 -0400

    commit this

--- guicode.py
+++ guicode.py
@@ -1,11 +1,19 @@
 #gui code
-import gw2botwserver
+from tkinter import *
+from tkinter import filedialog
+import tkinter
+import msvcrt
 import subprocess
 
 master = Tk()
 root = tkinter.Tk()
-root.withdraw()
-root.wm_attributes("-topmost",1)
+
+root.attributes("-topmost",True)
+master.withdraw()
+root.lift()
+account = open('account.txt', 'r+')
+accountline = account.readline()
+accountlinesplit = accountline.split(",")
 
 def saveusername():
     global user
@@ -24,9 +32,9 @@ def saveusername():
 def userpass():
     global ebutton
     global lbutton
-    ebutton = Entry(master)
+    ebutton = Entry(root)
     ebutton.pack()
-    lbutton = Entry(master)
+    lbutton = Entry(root)
     lbutton.pack()
     f.pack()
     
@@ -43,18 +51,25 @@ def gw2source():
     gw2locationfile= open('gw2location.txt', 'w').write(gw2location)
 
 def startgw2bot():
-    subprocess.call("gw2botwserver.pyw", shell=True)
+    l=0
+    while l == 0:
+        subprocess.call("gw2botwserver.pyw", shell=True)
+        root.update()
+        if msvcrt.kbhit():
+            if ord(msvcrt.getch()) == 59:
+                break
     
     
     
 
-b = Button(master, text="enter your username and password for the trading bot", command=userpass)
-f = Button(master, text="save your account details", command=saveusername)
-n = Button(master, text="Exit application", command = close)
-c = Button(master, text="Choose Gw2 location", command=gw2source)
-z = Button(master, text="Start Gw2 bot", command = startgw2bot)
+b = Button(root, text="enter your username and password for the trading bot", command=userpass)
+f = Button(root, text="save your account details", command=saveusername)
+n = Button(root, text="Exit application", command = close)
+c = Button(root, text="Choose Gw2 location", command=gw2source)
+z = Button(root, text="Start Gw2 bot", command = startgw2bot)
 
-
-
-
+c.pack()
+b.pack()
+z.pack()
+n.pack()
 mainloop()
--- gw2botwserver.pyw
+++ gw2botwserver.pyw
@@ -26,62 +26,6 @@ logging.basicConfig(
   format='%(asctime)s %(levelname)s %(message)s',
   level=logging.DEBUG,)
 
-#gui code
-
-master = Tk()
-root = tkinter.Tk()
-root.withdraw()
-
-def saveusername():
-    global user
-    user = ebutton.get()
-    accountlinesplit[0] = str(user)
-    print(user)
-    global password
-    password = lbutton.get()
-    accountlinesplit[1]= str(password)
-    print(password)
-    account = open('account.txt', 'r+').close()
-    account = open('account.txt', 'r+').write(accountlinesplit[0]+','+accountlinesplit[1])
-    
-
-
-def userpass():
-    global ebutton
-    global lbutton
-    ebutton = Entry(master)
-    ebutton.pack()
-    lbutton = Entry(master)
-    lbutton.pack()
-    f.pack()
-    
-
-def close():
-    root.quit()
-    
-def gw2source():
-    global root
-    global gw2location
-    gw2location = filedialog.askopenfilename()
-    gw2locationfile= open('gw2location.txt', 'w').close()
-    gw2locationfile= open('gw2location.txt', 'w').write(gw2location)
-    
-    
-    
-
-b = Button(master, text="enter your username and password for the trading bot", command=userpass)
-f = Button(master, text="save your account details", command=saveusername)
-
-c = Button(master, text="Choose Gw2 location", command=gw2source)
-z = Button(master, text="Start Gw2 bot", command = close)
-c.pack()
-
-b.pack()
-z.pack()
-
-mainloop()
-
-#end gui code
 
 server = xmlrpc.client.Server('https://'+user+':'+password+'@108.61.63.199:8000')
 print(server)
@@ -524,9 +468,6 @@ def gothroughbuyitems():
     
 
     for a, b, in zip(buyprice, unitprice):
-        if msvcrt.kbhit():
-            if ord(msvcrt.getch()) == 59:
-                break
         logging.error('poop = %s',poop)
         logging.error('pages = %s',pages)
         logging.error("xclicked %s", xcoordfirstitem)
